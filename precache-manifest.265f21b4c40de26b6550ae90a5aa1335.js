b0f9e02d267d276bee4b654aeee0649a39600724
upendranadh <n.upendranadh@gmail.com>
2020-08-22 00:14:27 +0530

    Updates

---
+++ precache-manifest.265f21b4c40de26b6550ae90a5aa1335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c74a4a2a52be4ef792397f58fb92a7c",
+    "url": "/ReactProject-1/index.html"
+  },
+  {
+    "revision": "990753de1a05b8f2fa6c",
+    "url": "/ReactProject-1/static/css/main.96e62b6b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/ReactProject-1/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactProject-1/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990753de1a05b8f2fa6c",
+    "url": "/ReactProject-1/static/js/main.d2c01bb9.chunk.js"
+  },
+  {
+    "revision": "05b6cc713e1e3a45af94",
+    "url": "/ReactProject-1/static/js/runtime-main.df54fa2c.js"
+  }
+]);
